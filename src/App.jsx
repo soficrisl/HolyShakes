e53442a8e0febed6a109909855f8a0c1810cfb84
unknown <arrue.julene@correo.unimet.edu.ve>
2024-06-19 22:31:31 -0400

    borrar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/nav/Header';
+
+
+function App({routes}) {
+
+  return (
+    <Home />
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+<<<<<<< HEAD
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+=======
+export default {
+  content: [],
+>>>>>>> 248c00c0e8799ccc749523df03bea5b25c13ac05
   theme: {
     extend: {},
   },
   plugins: [],
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 248c00c0e8799ccc749523df03bea5b25c13ac05
