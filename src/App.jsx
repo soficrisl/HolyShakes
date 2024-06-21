da48e01ce7177a687cd998ebdd2ec472b337dd49
ParraJuanPablo <parrajuanpalo@gmail.com>
2024-06-20 22:45:16 -0400

    Vite Project Functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Header from './components/nav/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navigation/Navbar";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-
-function App({routes}) {
-
+function App() {
   return (
     <div>
-    <Header/>
-    <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
-    
   );
 }
 
