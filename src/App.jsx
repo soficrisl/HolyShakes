838e759966bc6485b539d0258761b6904687abd0
Cristian Gouveia <cristian.gouveia@correo.unimet.edu.ve>
2024-06-22 19:37:19 -0400

    landing page 1/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navigation/Navbar";
@@ -5,11 +6,13 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import LandingSection from './components/navigation/landing/LandingSection';
+
 
 function App() {
   return (
-    <div>
-      <Router>
+    <Router>
+      <div className="App">
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -17,8 +20,9 @@ function App() {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
         </Routes>
-      </Router>
-    </div>
+        <LandingSection /> 
+      </div>
+    </Router>
   );
 }
 
