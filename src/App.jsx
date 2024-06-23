3e7d329ee52ad300f3be8c29f70ca400e35c3dee
ParraJuanPablo <parrajuanpalo@gmail.com>
2024-06-23 19:53:47 -0400

    #20 Arreglo Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import "./App.css";
 import Feedback from "./pages/Feedback";
 import Home from "./pages/Home";
 import Register from "./pages/Register"; 
+import Login from "./pages/Login"; 
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Home/>
-        {/*<Routes>
+        <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/menu" element={<Menu />} />
+          {/* <Route exact path="/menu" element={<Menu />} /> */}
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
-        </Routes>*/}
+        </Routes>
         </div>
       </Router>
 
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,19 +1,20 @@
 import logo from '../../assets/logo.png' ;
 import usuario from '../../assets/usuario.png'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header className="w-full h-12 p-16 flex justify-between items-center bg-creamhs">
       <nav className="flex space-x-4">
-        <a href="#" className="text-black dark:text-white">Inicio</a>
-        <a href="#" className="text-black dark:text-white">Menú</a>
-        <a href="#" className="text-black dark:text-white">Conócenos</a>
+        <Link to="/" className="text-black dark:text-white">Inicio</Link>
+        <Link to="/menu" className="text-black dark:text-white">Menú</Link>
+        <Link to="/aboutus" className="text-black dark:text-white">Conócenos</Link>
       </nav>
       <img src={ logo } alt="" className="h-28" />
-      <a href="#" className="text-black dark:text-white flex items-center">
+      <Link to="/login" className="text-black dark:text-white flex items-center">
         Iniciar Sesión
         <img src={ usuario } alt="" className="ml-2" />
-      </a>
+      </Link>
     </header>
   );
 };
