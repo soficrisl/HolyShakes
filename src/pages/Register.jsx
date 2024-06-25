d1f9de860115262ea2f79fa71444fb88497746ea
soficrisl <sleon@correo.unimet.edu.ve>
2024-06-24 23:01:23 -0400

    Changes on validations

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -29,30 +29,42 @@ const funcCreate = async(e) =>{
             <div className="flex place-content-evenly flex-col items-center gap-4 w-full h-full bg-gradient-circle ">
                 <h2 className="text-center text-4xl mt-4 uppercase tracking-widest text-orangehs">Registrate</h2>
                 <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl m-6 mt-0 p-6 bg-white dark:bg-gray-800 rounded-md shadow-lg backdrop-filter backdrop-blur-sm flex flex-col">
-                    <form className="w-full space-y-4" onSubmit={funcCreate}>
+                    <form className="w-full space-y-4 font-montserrat" onSubmit={funcCreate}>
                         <div>
-                            <label htmlFor="Fname" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Nombre</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                            htmlFor="Fname" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Nombre</label>
                             <input id="Fname" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" placeholder="Enter your first name" />
+                            <span className = "text-sm text-red-600"> El nombre debe tener entre 2-16 letras</span>
                         </div>
                         <div>
-                            <label htmlFor="Lname" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Apellido</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                            htmlFor="Lname" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Apellido</label>
                             <input id="Lname" placeholder="Enter your last name" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" />
+                            <span className = "text-sm text-red-600"> El apellido debe tener entre 2-16 letras</span>
                         </div>
                         <div>
-                            <label htmlFor="phone" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Telefono</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                            htmlFor="phone" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Telefono</label>
                             <input id="phone" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" placeholder="Enter your phone number" />
+                            <span className = "text-sm text-red-600"> El telefono debe tener entre 8-15 numeros</span>
                         </div>
                         <div>
-                            <label htmlFor="email" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Correo</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                            htmlFor="email" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Correo</label>
                             <input type="email" id="email" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" placeholder="Enter your email" />
+                            <span className = "text-sm text-red-600"> El correo debe ser Unimet </span>
                         </div>
                         <div>
-                            <label htmlFor="password" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Contraseña</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                             htmlFor="password" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Contraseña</label>
                             <input id="password" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" placeholder="Enter password" />
+                            <span className = "text-sm text-red-600"> Al menos 5 caracteres </span>
                         </div>
                         <div>
-                            <label htmlFor="checkpassword" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Confirmar Contraseña</label>
+                            <label /*value = {values.name} onChange={handleChange} onBlur = {handleBlur}*/
+                             htmlFor="checkpassword" className="block text-sm font-bold text-zinc-700 dark:text-zinc-300">Confirmar Contraseña</label>
                             <input id="checkpassword" className="mt-1 block w-full p-2 border border-zinc-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white" placeholder="Enter same password" />
+                            <span className = "text-sm text-red-600" > Las contraseñas deben ser iguales </span>
                         </div>
                         <button type="submit" className="w-full py-2 bg-orange-500 text-white rounded-md shadow-sm hover:bg-orange-600">Registrar</button>
                         <button type="button" className="w-full py-2 bg-white text-black border border-zinc-300 rounded-md shadow-sm flex items-center justify-center hover:bg-zinc-100 dark:bg-zinc-700 dark:text-white dark:border-zinc-600 dark:hover:bg-zinc-600">
