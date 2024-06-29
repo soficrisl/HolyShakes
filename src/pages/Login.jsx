55fbbf9ae4f39d9fa416443c317cc052324d1456
unknown <arrue.julene@correo.unimet.edu.ve>
2024-06-28 22:39:03 -0400

    Listo AboutUs reactive + detallitos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "../components/navigation/AboutUs"
 import Footer from "../components/navigation/Footer"
 import Mainlogin from "../components/navigation/Mainlogin"
 import Navbar from "../components/navigation/Navbar"
@@ -7,6 +8,7 @@ function Login() {
   return (
     <div className=" w-full h-full bg-gradient-circle ">
     <Navbar />
+    <AboutUs />
     <Mainlogin /> 
     <Footer/>
     </div>
