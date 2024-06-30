c5fe3ecc61b220951500b13360c8b427a54c1ca8
unknown <arrue.julene@correo.unimet.edu.ve>
2024-06-30 13:54:48 -0400

    Base MenuAdmin

--- src/pages/MenuAdmin.jsx
+++ src/pages/MenuAdmin.jsx
@@ -1,16 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import React, {useState} from 'react'
 import Footer from "../components/navigation/Footer"
 import Navbar from "../components/navigation/Navbar"
 import "../styles.css"
-import FoodDisplay from "../components/fooddisplay/FoodDisplay"
-import ExploreMenu from "../components/exploremenu/ExploreMenu"
-// eslint-disable-next-line no-unused-vars
-import SearchBar from '../components/searchbar/SearchBar'
-import RealizarPedido from '../components/realizarpedido/RealizarPedido'
-
 import FoodDisplayAdmin from "../components/fooddisplay-admin/FoodDisplay"
 import ExploreMenuAdmin from "../components/exploremenu-admin/ExploreMenu"
+// eslint-disable-next-line no-unused-vars
+import SearchBar from '../components/searchbar/SearchBar'
 
 function Menu() {
 
@@ -20,16 +14,9 @@ function Menu() {
     <>
       <div className=" w-full h-full bg-gradient-circle">
         <Navbar/>
-
         <ExploreMenuAdmin category={category} setCategory={setCategory}/>
         <FoodDisplayAdmin category={category}/>
         <Footer/>
-        
-        <ExploreMenu category={category} setCategory={setCategory}/>
-        <FoodDisplay category={category}/>
-        <RealizarPedido/>
-        <Footer/>
-
       </div>
     </>
   )
