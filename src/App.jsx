329764f27fa37b2a69268eaec23e29a4420f0692
ParraJuanPablo <parrajuanpalo@gmail.com>
2024-07-04 19:25:57 -0400

    Arreglo Usuario Perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Feedback from './pages/Feedback';
@@ -8,23 +8,27 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import LoginAdmin from './pages/LoginAdmin';
 import OrderPage from './pages/OrderPage';
+import Perfil from './pages/Perfil';
 import appFirebase from './credentials';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import MenuContextProvider from './components/context/MenuContext';
 import './styles.css';
 
-const auth = getAuth(appFirebase);
 
 function App() {
   const [usuario, setUsuario] = useState(null);
-
-  onAuthStateChanged(auth, (usuarioFirebase) => {
-    if (usuarioFirebase) {
-      setUsuario(usuarioFirebase);
-    } else {
-      setUsuario(null);
-    }
-  });
+  
+  useEffect(() => {
+    const auth = getAuth(appFirebase);
+    onAuthStateChanged(auth, (usuarioFirebase) => {
+      if (usuarioFirebase) {
+        setUsuario(usuarioFirebase);
+      } else {
+        setUsuario(null);
+      }
+    });
+  }, [])
+  
 
   return (
     <MenuContextProvider> {/* Envolver con MenuContextProvider */}
@@ -36,6 +40,7 @@ function App() {
             <Route exact path="/login" element={usuario ? <Navigate to="/" replace /> : <Login />} />
             <Route exact path="/register" element={usuario ? <Navigate to="/" replace /> : <Register />} />
             <Route exact path="/LoginAdmin" element={usuario ? <Navigate to="/" replace /> : <LoginAdmin />} />
+            <Route exact path="/perfil" element={<Perfil />} />
             <Route exact path="/feedback" element={<Feedback />} />
             <Route exact path="/order" element={<OrderPage />} />
           </Routes>
