01661292a3e7ce381982d8aed04ac79aa8b08a2c
camifermoso <131319548+camifermoso@users.noreply.github.com>
2024-07-09 12:19:37 -0400

    Update CompletedOrders.jsx

--- src/pages/CompletedOrders.jsx
+++ src/pages/CompletedOrders.jsx
@@ -4,7 +4,7 @@ import { useState,useEffect } from "react"
 import { getUsersOrders } from "../controllers/products";
 import Footer from "../components/navigation/Footer";
 import Navbar from "../components/navigation/Navbar";
-
+import NavbarAdmin from "../components/navigation/NavbarAdmin";
 
 export default function CompletedOrders() {
     
@@ -15,7 +15,7 @@ export default function CompletedOrders() {
 
     return (
         <div>
-            <Navbar></Navbar>
+            <NavbarAdmin></NavbarAdmin>
             <ChangeOrders/>
             <div className="p-2 py-4 sm:py-8 gap-y-4 px-8 flex flex-col h-auto min-h-screen justify-center items-center font-montserrat  text-xs sm:text-sm md:text-lg lg:text-xl">
             {pendingOrders.map((order, index) => (
